b01439d26d94ed4adcb953d70ec1031b85d0a32c
OmegaTopo <74385000+OmegaTopo@users.noreply.github.com>
2023-03-01 23:41:16 -0300

    Script CEP concluído

--- scriptCep.js
+++ scriptCep.js
@@ -1,15 +1,20 @@
 // API de CEP
+let cep;
+const botao = document.getElementById("input-pesquisa");
+
+botao.addEventListener("click", async function () {
+    cep = String(document.getElementById("cep").value);
+    await pesquisarCEP(cep);
+});
 
 
-
-const pesquisarCEP = async () => {
-    let cep = document.getElementById("cep").value;
-
-    if (cep.trim() === "") {
+const pesquisarCEP = async (cepe) => {
+    numero = Math.floor(Math.random() * 101);
+    if (cepe.trim() === "") {
         document.getElementById("resposta").innerHTML = "Digite um CEP válido";
     } else {
-        await fetch(`https://viacep.com.br/ws/${cep}/json/` , {
-            method: "GET",
+        await fetch(`https://viacep.com.br/ws/${cepe}/json/` , {
+            method: 'GET',
             headers: {
                 'content-type': 'application/json'
             }
@@ -20,29 +25,10 @@ const pesquisarCEP = async () => {
             return response.json();
         }).then((data) => {
             if(data.length === 0){
-                document.getElementById('resposta').innerHTML = `<h5>CEP não encontrado</h5>`
+                document.getElementById("resposta").innerHTML = `<h5>CEP não encontrado</h5>`
             } else {
-                document.getElementById("resposta").innerHTML = `CEP: ${data[0].cep} <br> , Logradouro: ${data[0].logradouro}<br> , Número: Math.random().toString(99), Bairro: ${data[0].bairro}<br>, Cidade: ${data[0].localidade}<br>, UF: ${data[0].uf}`;
+                document.getElementById("resposta").innerHTML = `<p> Endereço: ${data.logradouro}<br> , Número: ${numero}, Bairro: ${data.bairro}<br>, Cidade: ${data.localidade}<br>, UF: ${data.uf}</p>`;
             }
         });
     }
 }
-
-
-
-
-
-// Código da Cris
-/*`<div class="card" style="width: 18rem;">
-                <div class="card=body">
-                    <h5 class="card-title"><b>${data[0]['name']}</h5>
-                    </div>
-                    <ul class="list-group list-group-flush"
-                        <li class="list-group-item"><b>Logradouro: ${data[0]['logradouro']}</li>
-                        <li class="list-group-item"><b>Bairro: ${data[0]['bairro']}</li>
-                        <li class="list-group-item"><b>Cidade: ${data[0]['localidade']}</li>
-                        <li class="list-group-item"><b>Estado: ${data[0]['uf']}</li>
-                        <li class="list-group-item"><b>Numero: Math.random()}</li>
-                    </ul>
-                    </div>
-                </div> <hr/>` */
