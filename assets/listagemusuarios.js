0379396d05e7efe62417f880644e2acff928d5c0
Henrique Miranda <miranda.henrique@outlook.com.br>
2023-03-01 23:10:27 -0300

    listagem e busca de usuarios

--- assets/listagemusuarios.js
+++ assets/listagemusuarios.js
@@ -1,58 +1,56 @@
 let listaNomes = [];
-let listaCelular = [];
-let listaCPF = [];
-let numeroUsuarios = 15;
+let inputBusca;
+let numeroUsuarios = 10;
 
-const listagemUsuarios = async () => {
-    if (listaNomes.length === 0) {
-        await fetch(`https://geradorbrasileiro.com/api/faker/usuario?limit=${numeroUsuarios}`, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }).then((response) => {
-            if (!response.ok) {
-                throw new Error(`Erro de HTTP: ${response.status}`);
-            }
-            return response.json();
-        }).then((usuario) => {
+const campoUsuarios = document.getElementById("campo-usuarios");
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", async function () {
+  inputBusca = String(document.getElementById("input-busca").value);
+  await listagemUsuarios(inputBusca);
+});
+
+const listagemUsuarios = async (paramBusca) => {
+  if (listaNomes.length === 0) {
+    await fetch(
+      `https://geradorbrasileiro.com/api/faker/usuario?limit=${numeroUsuarios}`,
+
+      {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Erro de HTTP: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((usuario) => {
         listaNomes = usuario.values;
-    }) 
-    }
-}
 
-const listagemCelulares = async () => {
-    if (listaCelular.length === 0) {
-        await fetch(`https://geradorbrasileiro.com/api/faker/celular?limit=${numeroUsuarios}`, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }).then((response) => {
-            if (!response.ok) {
-                throw new Error(`Erro de HTTP: ${response.status}`);
-            }
-            return response.json();
-        }).then((celular) => {
-        listaCelular = celular.values;
-    }) 
-    }
-}
+        if (paramBusca) {
+          let listaNomesFiltrada = listaNomes.filter((nome) =>
+            nome.includes(paramBusca)
+          );
+          campoUsuarios.innerHTML = "";
+          for (let nome of listaNomesFiltrada) {
+            let elemento = document.createElement("li");
+            elemento.innerText = nome;
+            campoUsuarios.appendChild(elemento);
+          }
+        } else {
+          campoUsuarios.innerHTML = "";
+          for (let nome of listaNomes) {
+            let elemento = document.createElement("li");
+            elemento.innerText = nome;
+            campoUsuarios.appendChild(elemento);
+          }
+        }
 
-const listagemCPF = async () => {
-    if (listaCPF.length === 0) {
-        await fetch(`https://geradorbrasileiro.com/api/faker/cpf?limit=${numeroUsuarios}`, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }).then((response) => {
-            if (!response.ok) {
-                throw new Error(`Erro de HTTP: ${response.status}`);
-            }
-            return response.json();
-        }).then((cpf) => {
-        listaCPF = cpf.values;
-    }) 
-    }
-}
+        listaNomes = [];
+      });
+  }
+};
